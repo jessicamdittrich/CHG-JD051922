d95ac5a6762ee8c922d31f23c8c326fe179e63f6
Jessica Dittrich <104876367+jessicamdittrich@users.noreply.github.com>
2022-05-18 18:26:09 -0700

    Adding in html and javascript

---
+++ develop/script.js
@@ -0,0 +1,54 @@
+var getRandomSymbol = "!#$%&'()*+,-./:;<=>?@[]/^_`{|}~";
+var getRandomLower = "abcdefghijklmnopqrstuvwxyz"
+var getRandomUpper = "ABCDEFGHIJOLMNOPQRSTUVWXYZ"
+var getRandomNumber = "0123456789"
+var collectCharacters = ""; //If yes to above it collects all
+var passwordCollect = ""; //Randomly collects from collectCharacters
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword(); //password now equals passwordCollect
+  var passwordText = document.querySelector("#password"); //Where it's going to go
+  passwordText.value = password; //Password aka passwordCollect goes into here (.value removes within to replace with passwordCollect)
+}
+
+function generatePassword() {
+  var passwordLength = parseInt(window.prompt("How long would you like your password to be?"));
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    var upper = confirm("Do you want uppercase characters?");
+    var lower = confirm("Do you want lowercase characters");
+    var special = confirm("Do you want special characters");
+    var numbers = confirm("Do you want numbered characters");
+
+      if (upper) {
+        collectCharacters += getRandomUpper;
+        //console.log(collectCharacters);
+      } 
+      if (lower) {
+        collectCharacters += getRandomLower;
+      }
+      if (special) {
+        collectCharacters += getRandomSymbol;
+      }
+      if (numbers) {
+        collectCharacters += getRandomNumber;
+      }
+
+      for (var i = 0; i < passwordLength; i++){
+        var value = Math.floor(Math.random() * collectCharacters.length);
+        passwordCollect += collectCharacters[value] //Pulling the new random variables x10 within this loop
+        console.log(value);
+      }
+      console.log(passwordCollect);
+      return passwordCollect; //Forces passwork collect
+
+  } else {
+    alert("Please make sure your password is between 8-128 characters");
+  }
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword); //when I click generate button, function "writePassword" occurs
